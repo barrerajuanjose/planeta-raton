13e547139017cea758c70d716f0d517d65a2fde8
Juan Jose Barrera <barrerajuanjose@gmail.com>
2014-03-04 01:37:37 -0300

    fix a and fix sitemap

--- controllers/sitemapController.js
+++ controllers/sitemapController.js
@@ -5,7 +5,7 @@ exports.ping = function(req, res) {
 };
 
 exports.build = function(req, res) {
-	var sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>http://www.planetaraton.com.ar</loc></url><url><loc>http://www.planetaraton.com.ar/</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/economicos</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/moderados</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/lujosos</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/villas-de-lujo</loc></url>';
+	var sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>http://www.planetaraton.com.ar/</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/economicos</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/moderados</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/lujosos</loc></url><url><loc>http://www.planetaraton.com.ar/hoteles/categoria/villas-de-lujo</loc></url>';
 
 	var hotels = hotelsService.getAll();
 
